4e555cc4d420d873645dd1c79c06243824b62732
Gregory Bateham <gdubsthirteen@gmail.com>
2024-09-21 12:47:40 -0400

    First commit

---
+++ data/index.ts
@@ -0,0 +1,203 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Work Experience", link: "#experience" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize collaboration with clients, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zones",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack!",
+      description: "Just to name a few...",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Freelancing and working on personal projects",
+      description: "What am I up to?",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "NetSpider - Web-Scraping Application",
+      des: "Scrape specific web pages in search for posts containing keywords that have been linked to human trafficking.",
+      img: "/netspider2.png",
+      iconLists: ["/re.svg", "/javascript.png","/flask2.png", "/bootstrap2.png", "/python3.png", "/selenium.png", "/pandas.png"],
+      link: "https://github.com/PauloDrefahl/NetspiderHSI",
+    },
+    {
+      id: 2,
+      title: "PAT - AI Patent Checker",
+      des: "Simplify the process of making your patent original with PAT, an AI-powered patent checker.",
+      img: "/pat.png",
+      iconLists: ["/re.svg", "/tail.svg", "/dock.svg", "/javascript.png", "/python3.png", "/flask2.png", "/openapi.png"],
+      link: "https://github.com/zesutton2619/PAT",
+    },
+    {
+      id: 3,
+      title: "E-Commerce Website",
+      des: "Coming Soon...",
+      img: "/wip.png",
+      iconLists: [],
+      link: "",
+    },
+    {
+      id: 4,
+      title: "Banking Platform",
+      des: "Coming Soon...",
+      img: "/wip.png",
+      iconLists: [],
+      link: "",
+    },
+    {
+      id: 5,
+      title: "Barbershop Website",
+      des: "Coming Soon...",
+      img: "/wip.png",
+      iconLists: [],
+      link: "",
+    },
+    {
+      id: 6,
+      title: "Healthcare App Platform",
+      des: "Coming Soon...",
+      img: "/wip.png",
+      iconLists: [],
+      link: "",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Best Whitaker College of Engineering Design",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Freelance Web Developer",
+      desc: "Currently working with local barbershop Cut It Forward to develop their new website.",
+      className: "md:col-span-2",
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 2,
+      title: "Frontend Engineer Intern - (HSI)",
+      desc: "Worked with Homeland Security Investigations for 9 months to develop a web-scraping application as part of a senior project.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 3,
+      title: "JavaScript Foundations Professional Certificate",
+      desc: "Sponsored by Mozilla, this certificate is designed to help developers learn the fundamentals of JavaScript.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 4,
+      title: "Docker Foundations Professional Certificate",
+      desc: "Sponsored by Docker, this certificate is designed to help developers learn the fundamentals of Docker.",
+      className: "md:col-span-2",
+      thumbnail: "/dockerName.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/gdxbs"
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      link: "https://x.com/BatehamGregory"
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/gbateham/"
+    },
+  ];
